9097eda1ed015cb810f2d3dedd0a581f60f6b144
Jonathan Borja <jborja.one@gmail.com>
2024-11-30 20:20:47 -0700

    added actions and abilitites

--- FantasyGameSolution/Core/Jobs/Bard.cs
+++ FantasyGameSolution/Core/Jobs/Bard.cs
@@ -1,8 +1,7 @@
 ﻿using Core.Models;
-using Core.Actions.RangedActions;
 using Core.Interfaces;
-
-namespace Core.Jobs
+using Core.Actions.RangedActions;
+using Core.Actions.PhysicalAbilities;namespace Core.Jobs
 {
 	public class Bard : Job
 	{
@@ -12,7 +11,12 @@ namespace Core.Jobs
 			Description = "The word \"bard\" ordinarily puts folk in mind of those itinerant minstrels, fair of voice and nimble of finger, who earn their coin performing in taverns and the halls of great lords. Few know, however, that bards in fact trace their origins back to the bowmen of eld, who sang in the heat of battle to fortify the spirits of their companions.";
 			Actions = new List<IAction>
 			{
-				new HeavyShot()
+				new HeavyShot(),
+				new StraightShot(),
+				new BloodLetter()
+			};
+			Abilities = new List<IAbility>			{
+				new RagingStrikes()
 			};
 		}
 	}
--- FantasyGameSolution/Core/Jobs/WhiteMage.cs
+++ FantasyGameSolution/Core/Jobs/WhiteMage.cs
@@ -6,7 +6,7 @@ using Core.Interfaces;using Core.Actions.CasterAbilities;namespace Core.Jobs
 	public class WhiteMage : Job
 	{
 		public WhiteMage() { }        public override void Initialize()        {
-			JobId = 6;
+			JobId = 7;
 			Name = "White Mage";
 			Description = "White magic, the arcane art of succor, was conceived eras past that the world might know comfort. Alas, man began perverting its powers for self-gain, and by his wickedness brought about the Sixth Umbral catastrophe.";
 			Spells = new List<ISpell>
